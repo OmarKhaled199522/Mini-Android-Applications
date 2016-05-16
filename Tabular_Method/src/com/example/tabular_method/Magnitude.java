c451ea4c1f896a43220ed7f9df0b14efdaffaa3e
omarkhaled <omar.khaled2400@yahoo.com>
2016-05-16 03:44:14 +0200

    Android
    Android

--- Tabular_Method/src/com/example/tabular_method/Magnitude.java
+++
@@ -1,6 +0,0 @@
-package com.example.tabular_method;
-public class Magnitude {
-
-	public int term;
-}
-
--- Tabular_Method/src/com/example/tabular_method/Node.java
+++
@@ -1,9 +0,0 @@
-package com.example.tabular_method;
-
-public class Node {
-
-	public Object value;
-	public Node next;
-	public Node prev;
-}
-
